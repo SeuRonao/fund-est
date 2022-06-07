5a66669ab9b22aa52c7056b25c52752e8ea09185
SeuRonao <SeuRonao@users.noreply.github.com>
2022-06-07 11:40:33 +0000

    jekyll build from Action 692b76dc070b2307b42f95d57c3229afe0987244

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,109 @@
+var store = [{
+        "title": "Estatística Descritiva",
+        "excerpt":"Exercícios sobre Estatística Descritiva. Questão 1 Classifique as variáveis (qualitativa nominal, qualitativa ordinal, quantitativa discreta, quantitativa contínua): Vitamina (A, B1, B2, B6, B12). Quantidade de caloria na batata frita. Altura de uma pessoa. Desfecho de uma doença (curado, não curado). Classificação de uma lesão (lesão fatal; severa; moderada; pequena). Grupo...","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/exercises/estatistica-descritiva",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/graphs-500x300.jpg"
+      },{
+        "title": "Probabilidade e Variáveis Aleatórias",
+        "excerpt":"Exercícios sobre Probabilidade e Variáveis Aleatórias. As respostas da lista podem ser encontradas aqui. Tabela 1: Acidentes de Trânsito Para os exercícios a seguir utilize a seguinte tabela a respeito do uso de cinto de segurança em acidentes. Cinto/Acidente Letais Não Letais Com 3655 7005 Sem 4402 3040 Questão 1...","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/exercises/probabilidade-variaveis-aleatorias",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/rpg-dice-500x300.jpg"
+      },{
+        "title": "Inferência Estatística",
+        "excerpt":"Exercícios sobre inferência estatística. Questão 1 Em um programa de emagrecimento, 40 adultos usaram o método de Atkins para perder peso. Depois de 12 meses, o peso médio perdido foi de 1kg com desvio padrão de 2.1 kg. Construa um intervalo de confiança com 90% de nível de confiança para...","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/exercises/inferencia",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/magnifying-glass-500x300.jpg"
+      },{
+        "title": "Testes de Hipóteses",
+        "excerpt":"Exercícios sobre Teste de Hipóteses. Questão 1 A vida média de uma amostra de 100 lâmpadas de certa marca é de 1615 horas. Por similaridade com outros processos de fabricação, supomos o desvio padrão igual a 120 horas. Utilizando-se um nível de significância igual a 5%, desejamos testar se a...","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/exercises/testes-de-hipoteses",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/experiment-500x300.jpg"
+      },{
+        "title": "Regressão Linear Simples",
+        "excerpt":"Exercícios sobre Regressão Linear Simples. Em todos as questões abaixo encontre a reta que minimiza o quadrado dos erros. Questão 1 Pares $(x,y)$: $${(1, 2), (2, 1), (3, 3), (4, 5), (5, 4)}$$ Qual seria o valor de $y$ correspondente à $x = 0$? Questão 2 Pares $(x,y)$: $${(1, 1),...","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/exercises/regressao-linear",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/curves-abstract-500x300.jpg"
+      },{
+        "title": "Site com cara nova!",
+        "excerpt":"O site foi atualizado para ficar mais fácil, no futuro, de adicionar conteúdos e notícias como também para ficar mais fácil acompanhar as atualizações.  ","categories": ["site"],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/site/site-com-cara-nova/",
+        "teaser": "/fundamentos-de-estatistica/assets/images/fallback-teaser-500x300.jpg"
+      },{
+        "title": "Recebendo Notícias com RSS feed.",
+        "excerpt":"Para receber notificações de notícias novas no site sem ter que ficar verificando é suficiente instalar um leitor de RSS feed no navegador, ou um app no celular. Leitor de RSS via navegador Uma solução gratuita é o RSS feed reader para o Chrome. Comece instalando a extensão: Pode pular...","categories": ["tutorial"],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/tutorial/recebendo-not%C3%ADcias-com-rss-feed/",
+        "teaser": "/fundamentos-de-estatistica/assets/images/fallback-teaser-500x300.jpg"
+      },{
+        "title": "Ponto Facultativo.",
+        "excerpt":"Como consta na Portaria 430 do Ministério da Economia, o dia 01/11/2021 é ponto facultativo. Portanto, não teremos gravação, nem encontro síncrono na data mencionada.  ","categories": ["cronograma"],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/cronograma/ponto-facultativo/",
+        "teaser": "/fundamentos-de-estatistica/assets/images/fallback-teaser-500x300.jpg"
+      },{
+        "title": "Como Usar o Google Classroom",
+        "excerpt":"Para visualizar a turma do Google Classroom você deve primeiramente entrar no SIGAA e entrar na página da disciplina. Procure pelo seguinte menu na tela principal da disciplina: Possivelmente, aparecerá você verá a seguinte imagem: Escolha a sua conta institucional clicando na seta para baixo no menu. Caso você não...","categories": ["tutoriais"],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/tutoriais/como-usar-google-classroom/",
+        "teaser": "/fundamentos-de-estatistica/assets/images/fallback-teaser-500x300.jpg"
+      },{
+        "title": "Introdução Geral",
+        "excerpt":"Um apanhado geral dos conteúdos da disciplina. Apresentação da Disciplina Ementa; Método de avaliação; Motivação para estudo de estatística. Estatística Descritiva Um conjunto de técnicas para: coleta de dados; classificação de dados; apresentação de dados; interpretação e análise de dados. Estatística Descritiva Técnicas para resumir uma grande quantidade de dados...","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/subjects/introducao-geral",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/books-500x300.jpg"
+      },{
+        "title": "Estatística Descritiva",
+        "excerpt":"Como classificar variáveis, construir tabelas e desenhar gráficos. Variáveis Características de interesse da amostra ou população. Qualitativa: aquela que define uma qualidade sem valor numérico. Nominal: que não tem nenhuma relação de ordem. Exemplos: nome, cor, doente/sadio, esportista/sedentário. Ordinal: que tem alguma relação de ordem intrínseca. Exemplos: grau de escolaridade....","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/subjects/estatistica-descritiva",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/graphs-500x300.jpg"
+      },{
+        "title": "Medidas de Tendência Central",
+        "excerpt":"São medidas para resumir o miolo de uma distribuição de frequência em apenas um número. Para simplificar a notação, todo somatório na forma $\\sum_{i=1}^n$ será escrito como $\\sum$ quando o contexto dos itens a serem somados ficar claro. Somatórios Um somatório é uma forma de resumir uma sequencia de somas....","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/subjects/medidas-tendencia-central",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/numbers-500x300.jpg"
+      },{
+        "title": "Medidas de Dispersão",
+        "excerpt":"São medidas que tentam resumir, em apenas um número, a variabilidade do conjunto de dados analisados. Para simplificar a notação, todo somatório na forma $\\sum_{i = 1}^n$ será escrito como $\\sum$ quando o contexto dos itens a serem somados ficar claro. Amplitude É o maior valor menos o menor valor...","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/subjects/medidas-dispersao",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/cylinder-numbers-500x300.jpg"
+      },{
+        "title": "Probabilidade",
+        "excerpt":"Probabilidade é uma técnica matemática para se tentar estimar resultados de processos incertos. A palavra probabilidade deriva do Latim probare (provar ou testar). Informalmente, provável é uma das muitas palavras utilizadas para eventos incertos ou desconhecidos, sendo também substituída por algumas palavras como sorte, risco, azar, chance, incerteza, duvidoso, dependendo...","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/subjects/probabilidade",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/rpg-dice-500x300.jpg"
+      },{
+        "title": "Variáveis Aleatórias",
+        "excerpt":"É uma forma de resumir a parte interessante de um experimento aleatório. Também é uma ferramenta para facilitar a quantificação de eventos em valores numéricos. Exemplo Seja o experimento aleatório em que uma pessoa é sorteada ao acaso. Dependendo do estudo, pode-se ter interesse em saber a altura dessa pessoa,...","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/subjects/variaveis-aleatorias",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/gamble-500x300.jpg"
+      },{
+        "title": "Inferência Estatística",
+        "excerpt":"A inferência estatística é o nome que se dá ao conjunto de técnicas utilizadas para estimar o valor de uma característica de uma população baseado em algum valor de uma amostra dessa população. Definições População Conjunto de todos os dados a ser estudado. Parâmetro Uma característica da população. Amostra Parte...","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/subjects/inferencia-estatistica",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/magnifying-glass-500x300.jpg"
+      },{
+        "title": "Testes de Hipóteses",
+        "excerpt":"Testes de Hipóteses são técnicas que auxiliam a tomada de decisões confrontando duas hipóteses em busca de qual é a mais adequada.   Uma apresentação inicial sobre o tema pode ser encontrada aqui.  ","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/subjects/testes-hipoteses",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/experiment-500x300.jpg"
+      },{
+        "title": "Regressão Linear Simples",
+        "excerpt":"Técnica para encontrar uma relação entre um variáveis distintas.   Uma apresentação inicial pode ser encontrada aqui.  ","categories": [],
+        "tags": [],
+        "url": "/fundamentos-de-estatistica/subjects/regressao-linear-simples",
+        "teaser": "/fundamentos-de-estatistica/assets/images/teasers/curves-abstract-500x300.jpg"
+      }]
